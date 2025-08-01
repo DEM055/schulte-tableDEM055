c97826e4ba35b374b21d5729327aa22a162341f2
DEM055 <dem055@icloud.com>
2025-08-01 19:42:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Указываем базовый путь
 export default defineConfig({
-  base: '/schulte-tableDEM055/',
   plugins: [react()],
 })
